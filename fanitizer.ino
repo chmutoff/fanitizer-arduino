5078f36cacb394b082dad8f441b3f1719a92ad30
Anton <chmutoff@yandex.ru>
2020-07-08 07:50:22 +0200

    Added influxDB reporting and tacho reading

--- fanitizer.ino
+++ fanitizer.ino
@@ -1,9 +1,11 @@
 #include <SPI.h>
 #include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
-#include <Adafruit_Sensor.h>
+#include <WiFiUdp.h>
 #include <Adafruit_BME280.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_SSD1306.h>
+#include "ESP8266WiFi.h"
 
 #define SCREEN_WIDTH 128  // OLED display width, in pixels
 #define SCREEN_HEIGHT 64  // OLED display height, in pixels
@@ -15,20 +17,73 @@
 
 #define FAN_IN_PIN D6     // Intake fan PWM pin
 #define FAN_EX_PIN D7     // Exhaust fan PWM pin
+#define TACHO_IN_PIN D3   // Intake fan tacho pin
+#define TACHO_EX_PIN D4   // Exhaust fan tacho pin
 
 #define MIN_TEMP 25       // Temperature that triggers fan power on
 #define MAX_TEMP 35       // Temperature that triggers max fan power
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 Adafruit_BME280 bme;
+WiFiUDP udp;
+
 float temperature;        // Temperature read from sensor
 float humidity;           // Humidity read from sensor
 
 int fan_in_duty;          // Intake fan duty cycle value
 int fan_ex_duty;          // Exhaust fan duty cycle value
 
+volatile int inPulses;
+volatile int exPulses;
+unsigned long lastInRPMmillis = 0;
+unsigned long lastExRPMmillis = 0;
+
+const byte influxHost[] = {192, 168, 69, 96};   // Hostname of the server running influxDB
+const int influxPort = 8089;                    // UDP port number of the server running influxDB
+String nodeName = "NodeName";                   // Host tag reported to influxDB
+
+const char* ssid = "WiFi_SSID";
+const char* password = "************";
+
+ICACHE_RAM_ATTR void countInPulse() {
+  inPulses++;
+}
+
+ICACHE_RAM_ATTR void countExPulse() {
+  exPulses++;
+}
+
+int calculateInRPM() {
+  int RPM;  
+  noInterrupts();
+  int elapsedMS = (millis() - lastInRPMmillis)/1000;
+  int revolutions = inPulses / 2;
+  int revPerMS = revolutions / elapsedMS;
+  RPM = revPerMS * 60;
+  lastInRPMmillis = millis();
+  inPulses=0;
+  interrupts();
+  return RPM;
+}
+
+int calculateExRPM() {
+  int RPM;  
+  noInterrupts();
+  int elapsedMS = (millis() - lastExRPMmillis)/1000;
+  int revolutions = exPulses / 2;
+  int revPerMS = revolutions / elapsedMS;
+  RPM = revPerMS * 60;
+  lastExRPMmillis = millis();
+  exPulses=0;
+  interrupts();
+  return RPM;
+}
+
 void setup() {
     Serial.begin(115200);
+    delay(100);
+
+    Serial.println("setup()");
 
     // Initialize temperature sensor
     bme.begin(0x76);
@@ -36,7 +91,12 @@ void setup() {
     // Initialize PWM settings
     analogWriteFreq(25000); // 25KHz
     pinMode(FAN_IN_PIN, OUTPUT);
-    pinMode(FAN_EX_PIN, OUTPUT);    
+    pinMode(FAN_EX_PIN, OUTPUT);
+
+    pinMode(TACHO_IN_PIN, INPUT_PULLUP);
+    pinMode(TACHO_EX_PIN, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(TACHO_IN_PIN), countInPulse, RISING);
+    attachInterrupt(digitalPinToInterrupt(TACHO_EX_PIN), countExPulse, RISING);
 
     // Initialize display
     // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
@@ -44,7 +104,9 @@ void setup() {
       Serial.println(F("SSD1306 allocation failed"));
       for(;;); // Don't proceed, loop forever
     }
+
     showSplashScreen();
+    connectToWiFi(); // TODO: possible check if we are connected and add this function in each loop iteration or before sending data so we don't stuck with bad wifi
 }
 
 // Reboot device
@@ -67,7 +129,16 @@ void loop() {
    
     }
 
+    /*
+    Serial.print("IN RPM=");
+    Serial.println(calculateRPM(lastInRPMmillis, inPulses));
+
+    Serial.print("EX RPM=");
+    Serial.println(calculateRPM(lastExRPMmillis, exPulses));
+    */
+    
     displayInfo();
+    sendToInfluxdb();
     delay(5000);
 }
 
@@ -96,6 +167,49 @@ void displayInfo() {
     display.println(buffer);
       
     display.display();
+}
+
+void sendToInfluxdb(){
+  int inRPM = calculateInRPM();
+  int exRPM = calculateExRPM();
+  //lastExRPMmillis, exPulses
+
+  Serial.print("IN RPM=");
+  Serial.println(inRPM);
+  
+  Serial.print("EX RPM=");
+  Serial.println(exRPM);
+  //String line = String("environment,host=" + nodeName + " temperature=" + String(temperature) + ",humidity=" + String(humidity) + ",inRPM=" + String(inRPM) + ",exRPM=" + String(exRPM));
+  String line = String("environment,host=" + nodeName + " temperature=" + String(temperature) + ",humidity=" + String(humidity));
+  
+  Serial.print("Sending line: ");
+  Serial.println(line);
+
+  udp.beginPacket(influxHost, influxPort);
+  udp.print(line);
+  udp.endPacket();
+}
+
+void connectToWiFi(){
+    WiFi.mode(WIFI_STA); // Set ESP8266 mode to act as a Wi-Fi client and attempt to connect with credentials
+    WiFi.begin(ssid, password);
+    
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.println("WiFi connection failed!");
+        delay(500);
+    }
+    
+    Serial.println("Connected to network");
+    // print your WiFi shield's IP address:
+    IPAddress ip = WiFi.localIP();
+    Serial.print("IP Address: ");
+    Serial.println(ip);
+    
+    // print the received signal strength:
+    long rssi = WiFi.RSSI();
+    Serial.print("signal strength (RSSI):");
+    Serial.print(rssi);
+    Serial.println(" dBm");
 }
 
 // Draws an erecting penis :)
